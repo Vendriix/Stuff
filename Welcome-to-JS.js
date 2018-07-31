f1c581b8cd278689583f41a9d8722779c5c82ebf
Anghjula Dea Vincenti <vendrix17@gmail.com>
2018-07-31 11:39:30 +0200

    I'm learning JS it's pretty simple
    JavaScript is one of the most popular programming languages on earth and is used to add interactivity to web pages,
    process data, as well as create various applications (mobile apps, desktop apps, games, and more).

---
+++ Welcome-to-JS.js
@@ -0,0 +1,19 @@
+//By Vendrix 31/07/18
+
+//JavaScript is one of the most popular programming languages on earth
+//and is used to add interactivity to webpages,
+//process data, as well as create various applications
+//(mobile apps, desktop apps, games, and more)
+
+<html>
+   <head>
+     <title></title>
+   </head>
+   <body>
+    <script type="text/javascript">
+    
+      alert("This is an alert box!");
+
+     </script>
+   </body>
+</html>
